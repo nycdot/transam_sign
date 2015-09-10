8d268639f0852093886d8521fe5be6adf3f58bb0
Julian Ray <jray@camsys.com>
2015-09-10 09:24:50 -0400

    Add fixed/not-fixed scopes to support types

--- app/models/support_type.rb
+++ app/models/support_type.rb
@@ -3,6 +3,8 @@ class SupportType < ActiveRecord::Base
   default_scope { order(:name) }
 
   scope :active, -> { where(active: true) }
+  scope :fixed, -> { where(active: true, fixed: true) }
+  scope :not_fixed, -> { where(active: true, fixed: false) }
 
   def to_s
     name
