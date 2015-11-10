fb1f161870f4745e41634ce705d5ffb2993e7c00
Lydia Chang <mathmerized@gmail.com>
2015-11-10 11:54:11 -0500

    add config to test dummy app

--- spec/dummy/config/initializers/transam_core_extensions.rb
+++ spec/dummy/config/initializers/transam_core_extensions.rb
@@ -2,3 +2,6 @@
 # Defines services to use
 Rails.application.config.new_user_service = "NewUserService"
 Rails.application.config.policy_analyzer = "PolicyAnalyzer"
+
+# Set the image to use if a sign standard image is missing
+Rails.application.config.missing_sign_image = 'missing_sign_placeholder'
