a7c56fc148b4ffcfd29e96cf7100a3ce00353513
Lydia Chang <mathmerized@gmail.com>
2016-01-06 14:26:35 -0500

    cleanup test gems

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -6,6 +6,7 @@ require 'rspec/rails'
 require 'factory_girl_rails'
 require 'database_cleaner'
 require 'devise'
+require 'shoulda-matchers'
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
--- transam_sign.gemspec
+++ transam_sign.gemspec
@@ -24,5 +24,6 @@ Gem::Specification.new do |s|
   s.add_development_dependency "factory_girl_rails"
   s.add_development_dependency "database_cleaner"
   s.add_development_dependency "mysql2"
+  s.add_development_dependency "shoulda-matchers"
   s.add_development_dependency "cucumber-rails"
 end
