6d2f792df10262626c4c4f00a8c9218ab19f08b8
Lydia Chang <mathmerized@gmail.com>
2016-02-09 14:21:05 -0500

    rspec jobs

---
+++ spec/jobs/asset_mileage_update_job_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe AssetMileageUpdateJob, :type => :job do
+
+  class TestOrg < Organization
+    def get_policy
+      return Policy.where("`organization_id` = ?",self.id).order('created_at').last
+    end
+  end
+
+  let(:test_asset) { create(:service_vehicle) }
+
+  it 'sogr update' do
+    expect(AssetMileageUpdateJob.new(0).requires_sogr_update?).to be true
+  end
+
+  it '.run' do
+    test_event = test_asset.mileage_updates.create!(attributes_for(:mileage_update_event))
+    AssetMileageUpdateJob.new(test_asset.object_key).run
+    test_asset.reload
+
+    expect(test_asset.reported_mileage_date).to eql(Date.today)
+    expect(test_asset.reported_mileage).to eql(100000)
+  end
+
+  it '.prepare' do
+    test_asset.save!
+    allow(Time).to receive(:now).and_return(Time.utc(2000,"jan",1,20,15,1))
+    expect(Rails.logger).to receive(:debug).with("Executing AssetMileageUpdateJob at #{Time.now.to_s} for Asset #{test_asset.object_key}")
+    AssetMileageUpdateJob.new(test_asset.object_key).prepare
+  end
+end
