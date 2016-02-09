993fee066ddcbc4a0e36b87598325a715217aad5
Lydia Chang <mathmerized@gmail.com>
2016-02-09 13:32:21 -0500

    rename rspec helper

--- spec/helpers/transam_sign_helper_spec.rb
+++ spec/helpers/transam_sign_helper_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe TransamFormatHelper, :type => :helper do
+RSpec.describe TransamSignHelper, :type => :helper do
 
   it '.get_max_useful_miles_collection' do
     expect(helper.get_max_useful_miles_collection).to eq([['100,000', 100000], ['150,000', 150000], ['200,000', 200000], ['250,000', 250000], ['300,000', 300000],['350,000', 350000], ['400,000', 400000], ['450,000', 450000], ['500,000', 500000], ['550,000', 550000], ['600,000', 600000], ['650,000', 650000], ['700,000', 700000], ['750,000', 750000]])
