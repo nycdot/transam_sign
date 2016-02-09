1759990a0e27638fb00cb66daae38fa2b1908348
Lydia Chang <mathmerized@gmail.com>
2016-02-09 13:02:51 -0500

    finish up rspec lookups

---
+++ spec/models/fuel_type_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+
+RSpec.describe FuelType, :type => :model do
+
+  let(:test_type) { FuelType.first }
+
+  describe '#search' do
+    it 'exact' do
+      expect(FuelType.search(test_type.description)).to eq(test_type)
+    end
+    it 'partial' do
+      expect(FuelType.search(test_type.description[0..1], false)).to eq(test_type)
+    end
+  end
+
+  it '.to_s' do
+    expect(test_type.to_s).to eq(test_type.name)
+  end
+end
