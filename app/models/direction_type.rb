5e04382dbde241008a2e6c4803916a3c5f2cb75d
Xudong Liu <xudong.camsys@gmail.com>
2016-08-23 09:24:19 -0400

    Remove Unknown direction_type from seeding data; Add scope for arrow direction types [#128524927]

--- app/models/direction_type.rb
+++ app/models/direction_type.rb
@@ -3,6 +3,7 @@ class DirectionType < ActiveRecord::Base
   default_scope { order(:code) }
 
   scope :active, -> { where(active: true) }
+  scope :arrow_direction_types, -> { where(code: ['N', 'S', 'E', 'W']) }
 
   def to_s
     name
