838d44e5dcccc29b5ee3e954601698733b384d38
Xudong Liu <xudong.camsys@gmail.com>
2016-10-27 12:13:50 -0400

    Correct field name: fixed -> fixed_support

--- app/models/support_type.rb
+++ app/models/support_type.rb
@@ -3,8 +3,8 @@ class SupportType < ActiveRecord::Base
   default_scope { order(:name) }
 
   scope :active, -> { where(active: true) }
-  scope :fixed, -> { where(active: true, fixed: true) }
-  scope :not_fixed, -> { where(active: true, fixed: false) }
+  scope :fixed, -> { where(active: true, fixed_support: true) }
+  scope :not_fixed, -> { where(active: true, fixed_support: false) }
 
   def to_s
     name
